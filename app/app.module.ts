125018add84b2c2726fcaa2a8f9cd70acb7a009b
Blake Scala and Dean Scelza <blake@>
2016-10-17 15:43:21 -0700

    clean-up and refactor

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { PiesListComponent } from './pies-list.component';
+import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { PiesListComponent } from './pies-list.component';
 import { TaskListComponent } from './task-list.component';
 import { EditTaskComponent } from './edit-task.component';
 import { NewTaskComponent } from './new-task.component';
+import { CompletenessPipe } from './completeness.pipe';
+import { TaskComponent } from './task.component';
 
 @NgModule({
   imports: [
@@ -13,11 +15,13 @@ import { NewTaskComponent } from './new-task.component';
     FormsModule
   ],
   declarations: [
-    PiesListComponent,
     AppComponent,
+    PiesListComponent,
     TaskListComponent,
     EditTaskComponent,
     NewTaskComponent,
+    CompletenessPipe,
+    TaskComponent,
   ],
   bootstrap:    [ AppComponent ]
 })
--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -2,12 +2,18 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task.model';
 
+
 @Component({
   selector: 'task-list',
   template: `
-    <div  *ngFor="let currentTask of childTaskList">
-      <h3>{{ currentTask.description }}</h3>
-      <button (click)="editButtonHasBeenClicked(currentTask)"> Edit </button>
+    <select (change)="onChange($event.target.value)" class="filter">
+      <option value="all">Show All</option>
+      <option value="isDone">Show Done</option>
+      <option value="notDone" selected="selected">Show Not Done</option>
+    </select>
+    <div *ngFor="let currentTask of childTaskList | completeness:selectedCompleteness">
+      <task-display [task]="currentTask"></task-display>
+      <button (click)="editButtonHasBeenClicked(currentTask)">Edit</button>
     </div>
   `
 })
@@ -15,6 +21,11 @@ import { Task } from './task.model';
 export class TaskListComponent {
   @Input() childTaskList: Task[];
   @Output() clickSender = new EventEmitter();
+  public selectedCompleteness: string = "notDone";
+  onChange(optionFromMenu) {
+    this.selectedCompleteness = optionFromMenu;
+    console.log(this.selectedCompleteness);
+  }
   editButtonHasBeenClicked(taskToEdit: Task) {
     this.clickSender.emit(taskToEdit);
   }
